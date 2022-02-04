44b7ab0a27d9e5bff4f63a2e4be244b6e8fa6467
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-02-04 14:08:54 -0600

    modified function-prac js file

--- function-prac.js
+++ function-prac.js
@@ -1,8 +1,15 @@
 "use strict"
 
+alert("Welcome to Donut Disturb")
 //alert("Welcome to Donut Disturb")
 //Mini exercise #1
 //write a function named getName() which will return the result of the user inputting their name(s)
+//console log the invocation of getName() to ensure it works properly showName(username)
+//It takes in 'username' as an argument and alters the user to their name
+//Pass an invocation of getName() as an argument to the invocation of showName()
+//showName() should be void function. That is, it should not return a value - only execute code
+//Test to ensure your code is working as expected
+
 function getName(){
     return prompt("What is your preferred name?");
 }
@@ -11,13 +18,35 @@ function getName(){
 function showName(yoName){
     alert (yoName);
 }
-showName(getName()); // getNAme will return the user name and passes to showName
-//console log the invocation of getName() to ensure it works properly showName(username)
+showName(getName()); // getName will return the username and passes to showName
 
-//It takes in 'username' as an argument and alters the user to their name
+//Mini exercise #2
+// Make a function named isLowerCase(letter) which returns true if the parameter is lowercased;
+// //otherwise it returns false
+// Write a function named double(n) that returns a number times two
+// Make a function named areIdentical(input1, input2) that returns true if both inputs are same value and data type; otherwise it returns false.
+function isLowerCase(letter){
+    return letter == letter.toLowerCase() && letter != letter.toUpperCase();
+}
+console.log("Check: ", isLowerCase("Check"));
+console.log("check: ", isLowerCase("check"));
+console.log("ChEck: ", isLowerCase("ChEck"));
+console.log("CAPITAL: ", isLowerCase("CAPITAL"));
 
-//Pass an invocation of getName() as an argument to the invocation of showName()
+//Part 2 of exercise #2
+function double(n){
+    return n * 2;
+}
 
-//showName() should be void function. That is, it should not return a value - only execute code
+console.log("Check 2 * 2: ", double(2));
+console.log("Check 35.123 * 2: ", double(35.123));
+console.log("Check 281 * 2: ", double(281;
 
-//Test to ensure your code is working as expected
+//Part 3 of exercise #3
+function areIdentical(input1, input2){
+    return input1 === input2;
+}
+
+console.log("Check if One and one are identical: ", areIdentical("One", "one"));
+console.log("Check if one and one are identical: ", areIdentical("one", "one"));
+console.log("Check if one and 1 are identical: ", areIdentical("one", 1));
