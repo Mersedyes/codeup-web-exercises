ce1ba5b6453ee91820859402eac15cc9cac23266
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-02-07 14:31:47 -0600

    adding conditions-lecture practice

---
+++ js/conditions-lecture.js
@@ -0,0 +1,41 @@
+"use strict"
+
+//need to redo this whole thing....how frustrating
+function getToDestination(age, isInsured, hasCar, hasDriversNearby, hasEnoughMoney) {
+    // let age = 19;
+    // let isInsured = true;
+    // let hasCar = false;
+    // let canGetRideshare = true;
+
+//need to run through this again
+    // alternative is: (age && isInsured && hasCar){}
+    if (age >= 16 && isInsured === true && hasCar === true){
+        console.log("They see me rolling....they hatin");
+    } else if
+        //alternative is: else if (canGetRideshare){}
+        (age < 16 && isInsured === false || hasCar === false && canGetRideshare( hasDriversNearby, hasEnoughMoney)){
+        console.log("Use your bus pass or get an Uber...Lyft if you are woke");
+    } else {
+            console.log("You made all these plans with no where to get there...now look at you")
+    }
+}
+
+//the best option for part 2:
+function canGetRideshare(hasDriversNearby, hasEnoughMoney){
+    if (hasDriversNearby && hasEnoughMoney){
+        return true;
+    } else {
+        return false;
+    }
+}
+getToDestination(16, true, true, true, true );
+//
+//
+//
+// function canGetRideshare(age, driverNearBy, enoughMoney){
+//     if (driverNearBy && enoughMoney) {
+//
+//     }
+// }
+// getToDestination(16, false, false, true);
+
