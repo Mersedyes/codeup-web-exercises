ce7b58c101f963e8d6cb3061d1a9c4f623103ddc
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-02-09 16:09:37 -0600

    updated loops-prac.js and loops-prac.html

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,13 +1,13 @@
 // "use strict";
 // /* ########################################################################## */
-alert("Welcome to the Colorful World of Conditionals");
-
-let userInput;
-    userInput = prompt('Hello from Donut Disturb!');
-    console.log(userInput);
-
-let analyzeColor = prompt('What is your favorite color?');
-alert(`${userName}, ${analyzeColor} is my favorite color too!`);
+// alert("Welcome to the Colorful World of Conditionals");
+//
+// let userInput;
+//     userInput = prompt('Hello from Donut Disturb!');
+//     console.log(userInput);
+//
+// let analyzeColor = prompt('What is your favorite color?');
+// alert(`${userName}, ${analyzeColor} is my favorite color too!`);
 
 // /**
 //  * TODO:
@@ -29,39 +29,39 @@ alert(`${userName}, ${analyzeColor} is my favorite color too!`);
 //  * console.logging the function's return value
 //  */
 //
-function analyzeColor(colorInput) { //the parameter is colorInput; used to take in dynamic input (aka the unknown)
-    colorInput = colorInput.toLowerCase();
-
-    if (colorInput === "red"){
-        console.log("Strawberries are red")
-    } else if (colorInput === "blue"){
-        console.log("Blue is the color of the sky");
-    } else if (colorInput === "orange"){
-        console.log("Orange is the color of the horizon during a sunset");
-    } else if (colorInput === "yellow"){
-        console.log("Yellow like canaries and lemons");
-    } else if (colorInput === "green"){
-        console.log("Green like algae in a pond");
-    } else if (colorInput === "indigo"){
-        console.log("I don't really know what's indigo but it is a color");
-    } else if (colorInput === "Violet ") {
-        console.log("Violet like African Violets");
-    } else if (colorInput === "Black"){
-        console.log("Black like an onyx. Although black is the absorption of all colors");
-    } else if (colorInput === "White"){
-        console.log("White like the clouds on a sunny day");
-    } else (`Hmmmm....I didn't think of that ${colorInput}! How cool!`);
-}
-analyzeColor("red");
-analyzeColor("blue");
-analyzeColor("orange");
-analyzeColor("yellow");
-analyzeColor("green");
-analyzeColor("indigo");
-analyzeColor("violet");
-analyzeColor("black");
-analyzeColor("white");
-analyzeColor("randomColor");
+// function analyzeColor(colorInput) { //the parameter is colorInput; used to take in dynamic input (aka the unknown)
+//     colorInput = colorInput.toLowerCase();
+//
+//     if (colorInput === "red"){
+//         console.log("Strawberries are red")
+//     } else if (colorInput === "blue"){
+//         console.log("Blue is the color of the sky");
+//     } else if (colorInput === "orange"){
+//         console.log("Orange is the color of the horizon during a sunset");
+//     } else if (colorInput === "yellow"){
+//         console.log("Yellow like canaries and lemons");
+//     } else if (colorInput === "green"){
+//         console.log("Green like algae in a pond");
+//     } else if (colorInput === "indigo"){
+//         console.log("I don't really know what's indigo but it is a color");
+//     } else if (colorInput === "Violet ") {
+//         console.log("Violet like African Violets");
+//     } else if (colorInput === "Black"){
+//         console.log("Black like an onyx. Although black is the absorption of all colors");
+//     } else if (colorInput === "White"){
+//         console.log("White like the clouds on a sunny day");
+//     } else (`Hmmmm....I didn't think of that ${colorInput}! How cool!`);
+// }
+// analyzeColor("red");
+// analyzeColor("blue");
+// analyzeColor("orange");
+// analyzeColor("yellow");
+// analyzeColor("green");
+// analyzeColor("indigo");
+// analyzeColor("violet");
+// analyzeColor("black");
+// analyzeColor("white");
+// analyzeColor("randomColor");
 // //END OF THIS ACTIVITY
 // // // Don't change the next two lines!
 // // // These lines create two variables for you:
@@ -79,58 +79,61 @@ analyzeColor("randomColor");
 //  * TODO:
 //  * Comment out the code above, and refactor your function to use a switch-case statement
 //  */
-const analyzeColor = colorInput => {
-    colorInput = colorInput.toLowerCase();
-    switch (colorInput){
-        case "red"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        case "blue"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        case "orange"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        case "yellow"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        case "green"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        case "indigo"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        case "violet"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        case "black"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        case "white"
-        console.log(colorInput + ' is the perfect color I was thinking of too.');
-        break;
-
-        default:
-            console.log("I didn't think about that one, but it is the perfect color");
-            break;
-    }
-
-    analyzeColor(colorInput);
-}
+// const analyzeColor = colorInput => {
+//     colorInput = colorInput.toLowerCase();
+//     switch (colorInput){
+//         // alternative case 'red'
+//         case 'red':
+//         return (colorInput + ' is the perfect color I was thinking of too, like strawberries.');
+//         break;
+//
+//         case 'blue':
+//         return (colorInput + ' is the perfect color I was thinking of too, like the sky.');
+//         break;
+//
+//         case 'orange':
+//         return (colorInput + ' is the perfect color I was thinking of too, like the an orange.');
+//         break;
+//
+//         case 'yellow':
+//         console.log(colorInput + ' is the perfect color I was thinking of too, like daisies.');
+//         break;
+//
+//         case 'green':
+//         return (colorInput + ' is the perfect color I was thinking of too, like grass.');
+//         break;
+//
+//         case 'indigo':
+//         return (colorInput + ' is the perfect color I was thinking of too, like what is in the rainbow.');
+//         break;
+//
+//         case 'violet':
+//         return (colorInput + ' is the perfect color I was thinking of too, like African Violets.');
+//         break;
+//
+//         case 'black':
+//         return (colorInput + ' is the perfect color I was thinking of too, like an onyx stone.');
+//         break;
+//
+//         case 'white':
+//         return (colorInput + ' is the perfect color I was thinking of too, like clouds on a sunny day.');
+//         break;
+//
+//         default:
+//             return ("I didn't think about that one, but it is the perfect color");
+//             break;
+//     }
+//     console.log(analyzeColor('red'));
+// }
 // /**
 //  * TODO:
 //  * Prompt the user for a color when the page loads, and pass the input from the
 //  * user to your `analyzeColor` function. Alert the return value from your
 //  * function to show it to the user.
-//  */ SEE ABOVE (NEED TO CORRECT)
+//  */ DONE
+// let userColor = prompt("Please provide me a color.");
+// alert(analyzeColor(userColor)); //user input to pass the function
+
 
 // /* ########################################################################## */
 //
@@ -144,8 +147,50 @@ const analyzeColor = colorInput => {
 //  *
 //  * Write a function named `calculateTotal` which accepts a lucky number and total
 //  * amount, and returns the discounted price.
-function calculateTotal(luckyNumber, totalAmt, discountedPrice)
+let luckyNumber = Math.floor(Math.random() * 6);
+var userTotalBill = prompt("What is your total bill?");
 
+//user engagement prompts
+alert("Your lucky number is " + luckyNumber);
+alert("Your price before discount is $" + userTotalBill);
+alert("Your discounted price is: $" + calculateTotal(luckyNumber, userTotalBill));
+
+function calculateTotal(luckyNumber, totalAmt){ // parameters are luckyNumber and totalAmt
+   var discountPercentage; // initialize before the statement so that way the variable is available outside the block
+
+    // could use switch case but for practice used if else statements
+    if (luckyNumber === 0){
+        discountPercentage = 0.00;
+        //console.log("Sorry, no discount available.");
+    } else if (luckyNumber === 1){
+        discountPercentage = 0.10;
+        //console.log("Your lucky number has earned you 10% off your total purchase");
+    } else if (luckyNumber === 2){
+        discountPercentage = 0.25;
+        //console.log("Your lucky number has earned you 25% off your total purchase");
+    } else if (luckyNumber === 3){
+        discountPercentage = 0.35;
+        //console.log("Your lucky number has earned you 35% off your total purchase");
+    } else if (luckyNumber === 4){
+        discountPercentage = 0.45;
+        // console.log("Your lucky number has earned you 50% off your total purchase");
+    } else (luckyNumber === 5){
+        discountPercentage = 1.00;
+        // console.log("Call us Oprah, because your whole purchase is free!");
+    }
+    // only want to have to do the math once
+    var discount = totalAmt * discountPercentage; //
+    var discountedPrice = totalAmt - discount; // the math is mathing
+
+    return discountedPrice; // the return value
+}
+//simplified code, this is console.log since there is a return output
+console.log(calculateTotal(0, 100));
+console.log(calculateTotal(1,100));
+console.log(calculateTotal(2, 100));
+console.log(calculateTotal(3, 100));
+console.log(calculateTotal(4, 100));
+console.log(calculateTotal(5, 100));
 //  * Example:
 //  * calculateTotal(0, 100) // returns 100
 //  * calculateTotal(4, 100) // returns 50
@@ -164,7 +209,7 @@ function calculateTotal(luckyNumber, totalAmt, discountedPrice)
 //  * price before the discount was, and what their price after the discount is.
 //  */
 // // Generate a random number between 0 and 6
-// // var luckyNumber = Math.floor(Math.random() * 6);
+// //
 //
 // /**
 //  * TODO:
@@ -183,4 +228,33 @@ function calculateTotal(luckyNumber, totalAmt, discountedPrice)
 //  *
 //  * Can you refactor your code to use functions?
 //  * HINT: The way we prompt for a value could be improved
+//  * this will return a boolean (true or false) for the confirm making this a conditional
 //  */
+// STILL WORKING ON THIS
+var userChoice = confirm("Would you like to enter a number?");
+
+if (NaN(userNumber)){
+    alert("I\'m sorry this is not a number")
+} else {
+
+}
+
+
+
+
+if (userChoice){
+    var userNumber = parseInt(prompt("Enter a number")); // prompting user for number input and parsing it to a number
+    if (userNumber % 2 === 0){ // is the number even or odd if else statement
+        alert("Your number is even steven");
+    }//run the numbers
+} else {
+    alert("Your number is odd"); //dismissal of user
+}
+
+//plus 100
+alert("Your number plus 100 is: " + (userNumber + 100));
+
+// positive or negative
+if (userNumber > 0){
+    alert("Your number is proton positive")
+}
---
+++ js/loops-prac.js
@@ -0,0 +1,46 @@
+// TODO: Use a while look to output the numbers from 8-50
+// TODO: Using the same sample, output the ODD numbers from 8-50
+// TODO: HINT --> Use the % operator..
+
+// let num = 8;
+// while (num < 50){
+//     num++;
+//     if (num % 2 === 0){
+//         console.log(num);
+//     }
+// }
+
+// let fullNameEntered = false;
+// let names = "";
+//
+// //WE DONT KNOW HOW MANY NAMES THEY HAVE!
+// do {
+//     names += prompt("Please enter or add to your name");
+//     fullNameEntered = confirm("Is this your full name: " + names + "?")
+// }
+// alert("This is your name: " + names);
+
+// TODO: A parking lot with 30 parking spaces wants to display the amount of spaces available as it gets filled.
+//  Let's assume that they are allowing two cars into the parking lot at a time. As those cars take up spaces,
+//  write a do-while loop that prints the spaces available. If the parking lot gets filled, print "this parking lot
+//  is now full" to the console.
+//
+// let numSpaces = 30;{
+//     console.log("Welcome to the Heavily Priced Parking Garage");
+//     numSpaces -= 2;
+// } while (numSpaces)
+// console.log("The parking garage is full!");
+
+
+// TODO: using prompt() and alert(), write a while loop that will continue to ask for the correct password until the
+// //  correct password is given.
+// let password = "passwOrd123";
+// let isCorrect = false;
+// let attempt = "";
+// while (isCorrect) {
+//     attempt = prompt("Please enter your password");
+//     isCorrect = (attempt === password);
+//     if (!isCorrect){
+//         alert("aht aht wrong password...try again");
+//     }
+// }
