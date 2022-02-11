afb862ab702d51786e3bca0d0f27e9f917580a53
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-02-11 11:30:20 -0600

    created new file iterating_arrays_js.html and iterating.js. also updated break_and_continue.js file based upon feedback

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict;"
 
 // function randomNumber(max, min) {
 //     return Math.floor(Math.random() * (max - min) + min);
@@ -15,24 +15,24 @@
 // console.log("The loop is finished");
 
 
-let oddPrompt; //
-
-do {
-    oddPrompt = parseInt(prompt("Please enter an odd number between 1 and 50")) // parseInt turns a number into a floating
-    console.log("The number you have entered: " + oddPrompt + i);
-} while (oddPrompt % 2 !== 1 && oddPrompt < 1 || oddPrompt >= 50); // 1. is it odd 2. a number between 1 and 50
-
-console.log("Your number is: " + oddPrompt + " and is a valid odd number between 1 and 50");
-for (var i = 1; i <= 50; i++) {
-    if (i % 2 === 0) { // this skips even numbers
-        continue; // if the number is even, continue to the next iteration
-    }
-    if (oddPrompt === i) {
-        console.log("Uhhhh skipping this number: " + i);
-    } else {
-        console.log("Here is an odd number: " + i); // happy path neither even nor ordd
-    }
-}
+// let oddPrompt; //
+//
+// do {
+//     oddPrompt = parseInt(prompt("Please enter an odd number between 1 and 50")) // parseInt turns a number into a floating
+//     console.log("The number you have entered: " + oddPrompt + i);
+// } while (oddPrompt % 2 !== 1 && oddPrompt < 1 || oddPrompt >= 50); // 1. is it odd 2. a number between 1 and 50
+//
+// console.log("Your number is: " + oddPrompt + " and is a valid odd number between 1 and 50");
+// for (var i = 1; i <= 50; i++) {
+//     if (i % 2 === 0) { // this skips even numbers
+//         continue; // if the number is even, continue to the next iteration
+//     }
+//     if (oddPrompt === i) {
+//         console.log("Uhhhh skipping this number: " + i);
+//     } else {
+//         console.log("Here is an odd number: " + i); // happy path neither even nor ordd
+//     }
+// }
 
 // if (userChoice){
 //     var userNumber = parseInt(prompt("Enter an odd number")); // prompting user for number input and parsing it to a number
@@ -42,3 +42,42 @@ for (var i = 1; i <= 50; i++) {
 // } else {
 //     alert("Your number is odd"); //dismissal of user
 // }
+
+/* DOC ROB NOTES FOR HOW TO DO BREAK AND CONTINUE EXERCISE*/
+//declare variable called oddNumber and initialize it to promptForOddNumberInRange
+var oddNumber = promptForOddNumberInRange(1, 50);
+oddNumber = parseInt(oddNumber);
+
+printOddNumbersInRangeWithSkip(1, 50, oddNumber);
+
+function printOddNumbersInRangeWithSkip(low, high, skipNumber){
+    //Inclusive
+    for (let i = low; i <= high; i++){
+        if (i === skipNumber || i % 2 ===0){ // use an and when there is dependency, use || when they are independent of each other
+            //if i is the skipNumber then output something special
+            if (i === skipNumber){
+                console.log("Yikes! Skipping number: " + i);
+            }
+            continue; // moves to the next iteration
+        }
+        // if you get here then should be an odd number that is within range and not skipNumber
+        console.log("Here is an odd number: " + i);
+    }
+}
+
+function promptForOddNumberInRange(low, high, skipNumber) {
+    //loop until user gives an odd number in the range
+    while(true){
+        var result = prompt("Enter an odd number from " + low + " to " + high);
+        console.log(result);
+        if (result !== null && result % 2 === 1 && result >= low && result <= high) {
+            // the checks needing to be in place listed below:
+            // % is a remainder if odd its a remainder of 1 even would be 0
+            //need to know if a character or number was entered
+            //is the number entered in range?
+            break; // this break causes the closest contained loop to lead to the return result
+        }
+    }
+   // console.log(result); should not invisible outside of the function, not global
+    return result;
+}
---
+++ js/iterating.js
@@ -0,0 +1,126 @@
+// // "use strict;"
+// //
+// // //console.log("test")
+// //
+// // /*If your program is about dogs, you would not want to create a different variable for
+// // * each dog. Arrays allow you to make one variable where each of the elements is a dog
+// // * name. This allows the program to be more flexible without having to manipulate the
+// // * source code. Arrays are fundamental in programming, and all over the place.
+// // *
+// // * An array is a data structure that holds an ordered list of items, like a grocery store
+// // * list.*/
+// // // var dog1 = "Fido";
+// // // var dog2 = "Spot";
+// // // var dog3 = "Fifi";
+// // // var dog4 = "Lola";
+// //
+// // //descriptive function name. You declare an array with square brackets where on the inside you put the dog names
+// // var dogNames = ["Fido", "Spot", "Fifi", "Lola"]; // don't have to declare array on the onset always but we will in this case.
+// // var dogAges = [5, 2, 3, 10]; // order of elements in array is important and we must make those changes when needed
+// // var dogs = ["Fido"] //let you combine different elements (heterogenous elements) --> not good practice
+// // console.log("Dog names: " + dogNames); // output to the console of the array and concatonate
+// //
+// // /*arrays are complex data types, they have more stuff that will tell you about the array.
+// // * One of the fundamental properties of arrays is the length (returns the count of the
+// // * elements inside.*/
+// // // how to speak, accessing elements
+// // console.log(dogNames.length);
+// //
+// // //first element is at index 0 (zero based index)
+// // // note that index can also be referred to as a subscript
+// // console.log(dogNames[0]); // use a variable or a literal, should output Fido
+// //
+// // // log the 2nd element
+// // console.log(dogNames[1]);
+// //
+// // // log the 3rd element
+// // console.log(dogNames[dogNames.length - 2])
+// //
+// // // the last element is at index length - 1
+// // // how to speak accessing the last element
+// // // the last element in the array
+// // var lastIndex = dogNames.length - 1;
+// // console.log(dogNames[lastIndex]);// should output Lola and is concationated version of code above
+// //
+// // //loops and arrays are like peas and carrots
+// // for (var i = 0; i < dogNames.length; i++){// iterates 4 times and when i is 4 it stops; this is a counting loop
+// //     //this is the body
+// //     // i is the index, with control variables inside a loop just use one letter like 'i'
+// //     console.log("Dog at index: " + i + " is " + dogNames[i]);
+// // }
+// //
+// // //thus even index values will be considered an odd element
+// // for (let i = dogNames.length - 1; i >= 0; i--){
+// //     if (i % 2 === 0){
+// //         console.log("Dog at index " + i + " is " + dogNames[i]);
+// //     }
+// // }
+//
+// dogNames.forEach(function (dogName, index) { // this is a call back and needs a function
+//     //this function will be executed on each element
+//     // as the forEach iterates over the array
+//     // if(a certain record) {
+//     // skip that record
+//     // }
+//
+//     //dogName = "Zantor";
+//     dogNames
+//     // output the odd dogs (i.e...the dog at index 0 is an odd dog)
+//     if (index % 2 === 0) {
+//         console.log("Dog at index " + index + " is " + dogName);
+//     }
+// // // stop the loop with break if dogName is Spot
+// // if (dogName === "Spot") {
+// //     return;
+// // }
+// });
+//
+// console.log(dogNames);
+
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+})();
