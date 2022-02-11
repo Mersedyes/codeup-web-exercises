b815e5f632b4be2a88e3e9842181273f1ac7f775
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-02-11 09:03:47 -0600

    made revisions based upon feedback

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -14,23 +14,26 @@
 // }
 // console.log("The loop is finished");
 
-let oddPrompt = parseInt(prompt("Please enter an odd number between 1 and 50"))
-    console.log("The number you have entered: " + oddPrompt + i);
 
+let oddPrompt; //
+
+do {
+    oddPrompt = parseInt(prompt("Please enter an odd number between 1 and 50")) // parseInt turns a number into a floating
+    console.log("The number you have entered: " + oddPrompt + i);
+} while (oddPrompt % 2 !== 1 && oddPrompt < 1 || oddPrompt >= 50); // 1. is it odd 2. a number between 1 and 50
+
+console.log("Your number is: " + oddPrompt + " and is a valid odd number between 1 and 50");
 for (var i = 1; i <= 50; i++) {
-    console.log("Your number is: " + oddPrompt + " and is a valid odd number between 1 and 50");
-    if (i % 3 === 0) {
-        break;
+    if (i % 2 === 0) { // this skips even numbers
+        continue; // if the number is even, continue to the next iteration
     }
     if (oddPrompt === i) {
         console.log("Uhhhh skipping this number: " + i);
     } else {
-        console.log("")
-    }
-    if (i > 50 || ){
-        continue;
+        console.log("Here is an odd number: " + i); // happy path neither even nor ordd
     }
 }
+
 // if (userChoice){
 //     var userNumber = parseInt(prompt("Enter an odd number")); // prompting user for number input and parsing it to a number
 //     if (userNumber % 2 === 0){ // is the number even or odd if else statement
