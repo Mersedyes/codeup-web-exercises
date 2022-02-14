cc4ce1acf803f91ebae0dd785b54b0e7d60bb6d3
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-02-14 11:48:24 -0600

    created objects html and js files

---
+++ js/objects.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//don't use the construct, use object literal notation which is just the curly braces
+
+// CREATING AN OBJECT
+
+
+
+
+
+// ASSIGNING PROPERTIES TO AN OBJECT
+
+
+
+//ACCESSING PROPERTIES
+
+
+
+
+//FIND KEYS
