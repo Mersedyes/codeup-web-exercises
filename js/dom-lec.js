bd6d905db85ea66d4a4635230f4ce13913d19166
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-03-04 16:12:14 -0600

    completed todo and challenge

--- js/dom-lec.js
+++ js/dom-lec.js
@@ -1,11 +1,10 @@
-
 // get the thing
 let navContent = document.getElementById("navContent");
 
 // log the thing to make sure you got the thing
 // console.log(navContent);
 
-console.log(navContent.children);
+//console.log(navContent.children);
 
 // create an element
 let nameElement = document.createElement("p");
@@ -15,8 +14,12 @@ nameElement.innerText = getGitHubInfo().name;
 nameElement.style["color"] = 'white';
 console.log(nameElement)
 
-// TODO: Add nameElement to the DOM - specifically in between the GitHub Logo and the ul on the navbar
+//TODO: Add nameElement to the DOM - specifically in between the GitHub Logo and the ul on the navbar
+//treat this like an array
+navContent.insertBefore(nameElement, navContent.children[1]); // identify the children property and then the location using index
 
+//CHALLENGE: use the DOM to replace the github logo with octocat
+document.getElementById("github").src = "Octocat-from-github.png";
 
 
 
