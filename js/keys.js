b712e293836073ea02c4d08e19546777e7c9903b
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-03-21 14:22:41 -0500

    created a js file for headmap

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,4 @@
 const OWM_Key = "6f8469087eee47719da53a754e47ecdb";
 const MAP_Key = "pk.eyJ1IjoibWVyc2VkeWVzIiwiYSI6ImNsMHZkNmIwdjEzYjgzaXJwaGVhcjgyeXMifQ.luZ-V3XuxjWWqqUQEqUY4g;"
+
+const Weather_Map_Key = "pk.eyJ1IjoibWVyc2VkeWVzIiwiYSI6ImNsMTEyc3M5bjJxYW8zZHFvaTJ0dHM5Z2sifQ.zgQmubthMYfaD8peb4dk_w"
