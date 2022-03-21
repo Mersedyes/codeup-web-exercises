efcb07b7611fbc3b5ea67d62b0d3316af115b0ba
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-03-21 08:20:24 -0500

    almost completed this exercise, need to figure out the setLngLat and center part

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,3 +1,28 @@
 geocode('San Antonio', mapboxtoken).then(function (result){
     console.log(result);
+    mapboxgl.accessToken = mapboxtoken;
+
+    var map = new mapboxgl.Map ({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 15,
+        center: ([,])
+    });
+
+    var markerProperties = {
+        color = 'blue',
+        draggable: true
+    };
+
+    var marker = new mapboxgl.Marker(markerProperties)
+        .setLngLat([,])
+        .addTo(map);
+
+    map.on('click', function (){
+        var popup = new mapboxgl.Popup()
+            .setHTML("<h1>CodeUp is Legit</h1>")
+            .addTo(map);
+
+        marker.setPopup(popup);
+    });
 });
