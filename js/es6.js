4e504a2bf41dbc6356db7c82e7d95a84d862ff05
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-03-23 09:22:28 -0500

    began working in es6.js file, created and completed map-filter-reduce.js with Ry

--- js/es6.js
+++ js/es6.js
@@ -30,7 +30,8 @@ const users = [
 ];
 
 // TODO: fill in your name and email and add some programming languages you know
-// to the languages array
+
+
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
 var name = 'your_name_here';
